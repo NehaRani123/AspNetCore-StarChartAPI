5f866c8d6fae6ac3052e31e7bba8a03129490aad
NehaRani123 <96674448+NehaRani123@users.noreply.github.com>
2021-12-26 13:07:39 +0530

    GetActions Module completed

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using StarChart.Data;
+using StarChart.Models;
 
 namespace StarChart.Controllers
 {
@@ -17,5 +18,58 @@ namespace StarChart.Controllers
             _context = dbContext;
         }
         
+        [HttpGet("{id:int}")]
+        [Route("GetById/{id}")]
+        public IActionResult GetById(int id)
+        {
+            var data = _context.CelestialObjects.Where(x => x.Id == id).FirstOrDefault();
+            if(data is null)
+            {
+                return new NotFoundResult();
+            }
+            else
+            {
+                data.Satellites = new List<CelestialObject>() { data };
+                return new OkObjectResult(data);
+            }
+        }
+
+        [HttpGet("{name}")]
+        [Route("GetByName/{name}")]
+        public IActionResult GetByName(string name)
+        {
+            var data = _context.CelestialObjects.Where(x => x.Name == name).ToList();
+            if (data.Count == 0)
+            {
+                return new NotFoundResult();
+            }
+            else
+            {
+                foreach (var item in data)
+                {
+                    item.Satellites = new List<CelestialObject>() { item };
+                }
+                return new OkObjectResult(data);
+            }
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var data = _context.CelestialObjects.ToList();
+            if (data.Count == 0)
+            {
+                return new NotFoundResult();
+            }
+            else
+            {
+                foreach (var item in data)
+                {
+                    item.Satellites = new List<CelestialObject>() { item };
+                }
+                return new OkObjectResult(data);
+            }
+        }
+
     }
 }
